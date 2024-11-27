e2ce6c98f4c7b28444ec5c7248a060190fe4422c
Herbert Asis <herbert.asis@gcash.com>
2024-11-27 14:40:28 +0800

    image

--- src/components/propertyReportTable.tsx
+++ src/components/propertyReportTable.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable prettier/prettier */
 /* eslint-disable tailwindcss/no-custom-classname */
@@ -6,27 +7,78 @@
 import "datatables.net-dt/css/dataTables.dataTables.min.css";
 import "../extension.css";
 import { useEffect, useState } from "react";
-import DataTable from "datatables.net-dt";
+import DataTable from "datatables.net-react";
+import DT from "datatables.net-dt";
 import { useDispatch, useSelector } from "react-redux";
 import { getPropertyReportsReducer } from "../store/features/reducers";
 import { useParams } from "react-router";
 import type { Report } from "../types";
 import { type PropertyState } from "../types";
-import { Button } from "flowbite-react";
+import { Button, Dropdown } from "flowbite-react";
 import { BsThreeDots } from "react-icons/bs";
+DataTable.use(DT);
 const PropertyReportTable = function ({ headerValue }: any) {
   const { project_id }: any = useParams();
   const { propertyReports }: PropertyState = useSelector(
     (state: any) => state.property
   );
-  const [reports, setReports] = useState<any[]>([]);
-  const [isInit, setIsInit] = useState<boolean>(false);
+  const [reports, setReports] = useState<any[] | undefined>(undefined);
   const dispatch = useDispatch();
+  const [tableData, setTableData] = useState<any>([]);
 
   useEffect(() => {
-    try {
-      if (!DataTable.isDataTable("#property-report-table") && isInit) {
-        new DataTable("#property-report-table", {
+    const rp =
+      reports &&
+      reports.length > 0 &&
+      reports.map((r: Report) => {
+        return [
+          r.lotNo,
+          r.unitNo,
+          r.owners &&
+            r.owners.length > 0 &&
+            r.owners.map((o) => o.fullName).join(", "),
+        ];
+      });
+    setTableData(rp);
+  }, [reports]);
+
+  useEffect(() => {
+    dispatch(getPropertyReportsReducer(project_id));
+  }, []);
+
+  useEffect(() => {
+    setReports([]);
+    if (propertyReports) {
+      const rep =
+        propertyReports && propertyReports.find((o) => o.key == headerValue);
+      setReports(rep && rep.reports ? rep.reports : []);
+    }
+  }, [propertyReports, headerValue]);
+
+  return (
+    <>
+      <DataTable
+        className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400"
+        slots={{
+          3: (data: any, row: any) => (
+            <Dropdown
+              label=""
+              dismissOnClick={false}
+              renderTrigger={() => (
+                <Button color="gray" className="w-[50px]">
+                  <div className="flex items-center gap-x-2 text-xs">
+                    <BsThreeDots />
+                  </div>
+                </Button>
+              )}
+            >
+              <Dropdown.Item>View</Dropdown.Item>
+            </Dropdown>
+          ),
+        }}
+        data={tableData}
+        options={{
+          destroy: true,
           paging: true,
           searching: false,
           layout: {
@@ -39,83 +91,23 @@ const PropertyReportTable = function ({ headerValue }: any) {
             },
             bottomEnd: "paging",
           },
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [isInit]);
-
-  useEffect(() => {
-    dispatch(getPropertyReportsReducer(project_id));
-  }, []);
-
-  useEffect(() => {
-    setReports([]);
-    if (propertyReports) {
-      const rep =
-        propertyReports && propertyReports.find((o) => o.key == headerValue);
-      setReports(rep && rep.reports ? rep.reports : []);
-
-      setTimeout(() => {
-        setIsInit(true);
-      }, 1000);
-    }
-  }, [propertyReports, headerValue]);
-
-  return (
-    <>
-      {isInit && (
-        <table
-          id="property-report-table"
-          className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400"
-        >
-          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="w-[40%] px-6 py-3">
-                LOT NO.
-              </th>
-              <th scope="col" className="w-[40%] px-6 py-3">
-                UNIT NO.
-              </th>
-              <th scope="col" className="w-[40%] px-6 py-3">
-                OWNER NAME
-              </th>
-              <th scope="col" className="px-6 py-3"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {reports &&
-              reports.length > 0 &&
-              reports.map((r: Report, index) => {
-                return (
-                  <tr
-                    key={index}
-                    className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
-                  >
-                    <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
-                      {r.lotNo}
-                    </td>
-                    <td className="px-6 py-4">{r.unitNo}</td>
-                    <td className="px-6 py-4">
-                      {r.owners &&
-                        r.owners.length > 0 &&
-                        r.owners.map((o) => o.fullName).join(", ")}
-                    </td>
-                    <td className="px-6 py-4">
-                      <Button color="gray" className="w-[50px]">
-                        <div className="flex items-center gap-x-2 text-xs">
-                          <BsThreeDots />
-                        </div>
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      )}
-      {/* </div> */}
+        }}
+      >
+        <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              LOT NO.
+            </th>
+            <th scope="col" className="px-6 py-3">
+              UNIT NO.
+            </th>
+            <th scope="col" className="px-6 py-3">
+              OWNER NAME
+            </th>
+            <th></th>
+          </tr>
+        </thead>
+      </DataTable>
     </>
   );
 };
