aeb07c6249e8f87377ae20e7df8643e9b4d2e885
Herbert Asis <herbert.asis@gcash.com>
2024-12-11 07:39:01 +0800

    fix add

--- src/components/modals/commonAreaConfigModal.tsx
+++ src/components/modals/commonAreaConfigModal.tsx
@@ -436,9 +436,15 @@ export const CommonAreaConfigModal = function (props: any) {
                         </svg>
 
                         <span>
-                          {allCommonArea &&
-                            allCommonArea.length &&
-                            allCommonArea.find((ca) => ca.id == si)?.name}
+                          {allCommonArea && allCommonArea.length ? (
+                            allCommonArea.find((ca) => ca.id == si)?.name
+                          ) : (
+                            <div className="flex w-full items-center">
+                              <div className="h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                              <div className="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+                              <div className="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+                            </div>
+                          )}
                         </span>
                       </div>
                     );
