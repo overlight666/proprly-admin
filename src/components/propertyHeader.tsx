6f9326ed42ee71d103b72bcd65ddd7e08bada345
Herbert Asis <herbert.asis@gcash.com>
2025-01-06 06:02:41 +0800

    dashboard

--- src/components/propertyHeader.tsx
+++ src/components/propertyHeader.tsx
@@ -12,7 +12,12 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { AiOutlineRight } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
-import type { ProjectState, TowerData } from "../types";
+import type {
+  OrgState,
+  ProjectState,
+  PropertyState,
+  TowerData,
+} from "../types";
 import { getTowersReducer } from "../store/features/reducers";
 const PropertyHeader = function () {
   const [openModal, setOpenModal] = useState(false);
@@ -20,8 +25,16 @@ const PropertyHeader = function () {
   const { id, project_id }: any = useParams();
   const dispatch = useDispatch();
 
-  const { projectTowers }: ProjectState = useSelector(
+  const { projectTowers, selectedProject }: ProjectState = useSelector(
     (state: any) => state.project
+  );
+
+  const { selectedProperty }: PropertyState = useSelector(
+    (state: any) => state.property
+  );
+
+  const { selectedOrganization }: OrgState = useSelector(
+    (state: any) => state.organization
   );
 
   useEffect(() => {
@@ -40,28 +53,45 @@ const PropertyHeader = function () {
   };
 
   const generateTemplate = async () => {
-    const cols = projectTowers.map((t: TowerData) => {
+    const nextChar = (c) => {
+      return String.fromCharCode(c.charCodeAt(0) + 1);
+    };
+    const cols: any = projectTowers.map((t: TowerData) => {
       return {
-        name: t.name,
+        name: t.name.replace(/ /g, "_"),
       };
     });
+
+    const towerName = projectTowers.map((t: TowerData) => t.name);
 
     const contents: any = projectTowers.map((t: TowerData) => {
       return t.floorList;
     });
 
-    const result: any = [];
-
-    const colValues = contents.map((value, index) => {
-      result.push(contents[index]);
+    const result: any[] = [];
+    let len = 0;
+    contents.map((value, index) => {
+      if (len < value.length) {
+        len = value.length;
+      }
     });
 
-    console.log(result);
+    for (let i = 0; i < len; i++) {
+      const arr: any = [];
+      for (let x = 0; x < contents.length; x++) {
+        if (contents[x][i]) {
+          arr.push(contents[x][i]["value"]);
+        } else {
+          arr.push("");
+        }
+      }
+      result.push(arr);
+    }
 
     const workbook = new excelJs.Workbook();
 
-    const ws: any = workbook.addWorksheet("Test Worksheet");
-
+    const ws: any = workbook.addWorksheet("Template");
+    const lists = workbook.addWorksheet("List");
     const options1 = ["Pre-Settlement", "Handover", "Post-Handover"];
     const options2 = ["O3", "O4"];
     const options3 = ["O5", "O6"];
@@ -85,33 +115,61 @@ const PropertyHeader = function () {
 
     ws.columns.map((col, index) => (col.width = 18));
 
+    lists.addTable({
+      name: "Towers",
+      ref: "A1",
+      headerRow: true,
+      totalsRow: false,
+      columns: [{ name: "Towers" }],
+      rows: towerName.map((r) => [r.replace(/ /g, "_")]),
+    });
+
+    lists.addTable({
+      name: "Floors",
+      ref: "B1",
+      headerRow: true,
+      totalsRow: false,
+      columns: cols,
+      rows: result,
+    });
+
+    workbook.definedNames.add("List!$A$2:$A$100", "Towers");
+
+    let ncar = "A";
+    projectTowers.map((o) => {
+      ncar = nextChar(ncar);
+      workbook.definedNames.add(
+        `List!$${ncar}2:$${ncar}$100`,
+        o.name.replace(/ /g, "_")
+      );
+    });
+
     ws.dataValidations.add("C2:C99999", {
       type: "list",
       allowBlank: false,
       formulae: [`"${options1.join(",")}"`],
     });
 
-    ws.addTable({
-      name: "Towers",
-      ref: "O1",
-      headerRow: true,
-      totalsRow: true,
-      columns: cols,
-      rows: [
-        [new Date("2019-07-20")],
-        [new Date("2019-07-21")],
-        [new Date("2019-07-22")],
-      ],
+    ws.dataValidations.add("D2:D99999", {
+      type: "list",
+      allowBlank: false,
+      formulae: ["Towers"],
     });
-    // ws.getCell("B2:B99999").value = {
-    //   formula: 'IF(datasheet!A1<>"",datasheet!B1,"")',
-    // };
 
+    ws.dataValidations.add("E2:E99999", {
+      type: "list",
+      allowBlank: false,
+      formulae: ["INDIRECT(D2)"],
+    });
     // ws.dataValidations.add("B2:B99999", {
     //   type: "list",
     //   allowBlank: false,
-    //   formulae: [`IF(A1="O1","${options2.join(",")}","${options3.join(",")}")`],
+    //   source: "INDIRECT(02)",
     // });
+
+    // ws.getCell("D1").value = {
+    //   formula: "$D$1:$E$1",
+    // };
 
     // ws.dataValidations.add("B2:B99999", {
     //   type: "list",
@@ -150,14 +208,14 @@ const PropertyHeader = function () {
       })
     );
 
-    // const link = document.createElement("a");
-    // link.href = excelUrl;
-    // link.download = "herbert2.xlsx";
-    // document.body.appendChild(link);
-    // link.click();
+    const link = document.createElement("a");
+    link.href = excelUrl;
+    link.download = `${selectedOrganization?.name}_${selectedProject?.name}_unit_${selectedProperty?.unitNo}_lot_${selectedProperty?.lotNo}_template.xlsx`;
+    document.body.appendChild(link);
+    link.click();
 
-    // URL.revokeObjectURL(excelUrl);
-    // document.body.removeChild(link);
+    URL.revokeObjectURL(excelUrl);
+    document.body.removeChild(link);
   };
 
   return (
