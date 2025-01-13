99b70b81eb64c8ea6be1d4b47e5e1c51da396af6
Herbert Asis <herbert.asis@gcash.com>
2025-01-14 07:14:35 +0800

    master config

--- src/pages/admin/items/configure.tsx
+++ src/pages/admin/items/configure.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Accordion, Button } from "flowbite-react";
 import DefectCodeManagement from "./configureItems/codeManagement";
@@ -16,26 +17,22 @@ export default function ConfigureAccordion({ project_id }: any) {
         <Accordion.Panel>
           <Accordion.Title>Defect Code Management</Accordion.Title>
           <Accordion.Content>
-            <DefectCodeManagement project_id={project_id} />
+            {/* <DefectCodeManagement project_id={project_id} /> */}
           </Accordion.Content>
         </Accordion.Panel>
         <Accordion.Panel>
           <Accordion.Title>Trade & Defect Code Mapping</Accordion.Title>
           <Accordion.Content>
-            <TradeMapping project_id={project_id} />
+            {/* <TradeMapping project_id={project_id} /> */}
           </Accordion.Content>
         </Accordion.Panel>
         <Accordion.Panel>
           <Accordion.Title>Property Checklist Configuration</Accordion.Title>
-          <Accordion.Content>
-            <PropertyChecklist />
-          </Accordion.Content>
+          <Accordion.Content>{/* <PropertyChecklist /> */}</Accordion.Content>
         </Accordion.Panel>
         <Accordion.Panel>
           <Accordion.Title>Common Area Checklist Configuration</Accordion.Title>
-          <Accordion.Content>
-            <CommonArea />
-          </Accordion.Content>
+          <Accordion.Content>{/* <CommonArea /> */}</Accordion.Content>
         </Accordion.Panel>
       </Accordion>
       <div className="my-5 flex flex-row gap-5">
