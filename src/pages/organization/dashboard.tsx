fd876f4680a5ba06863941184a1da905c7c626b2
Herbert Asis <herbert.asis@gcash.com>
2025-02-06 16:14:48 +0800

    resched

--- src/pages/organization/dashboard.tsx
+++ src/pages/organization/dashboard.tsx
@@ -289,7 +289,10 @@ const Dashboard: FC = function () {
                           <div className="font-semibold text-gray-900 dark:text-white">
                             {notif.title}
                             <div className="ml-5 inline-block rounded-md bg-blue-100 p-1 text-xs font-medium text-primary-700 dark:text-primary-400">
-                              {moment(notif.createdAt, "YYYYMMDD").fromNow()}
+                              {moment
+                                .utc(notif.createdAt, "YYYYMMDD")
+                                .local()
+                                .fromNow()}
                             </div>
                           </div>
                         </Timeline.Time>
@@ -346,10 +349,10 @@ const Dashboard: FC = function () {
                               Date:{" "}
                               <span className="text-gray-600">
                                 {" "}
-                                {moment(
-                                  notif.createdAt,
-                                  "YYYY-MM-DD h:mm:ss a"
-                                ).format("MMM Do, YYYY h:mm:ss a")}
+                                {moment
+                                  .utc(notif.createdAt, "YYYY-MM-DD h:mm:ss a")
+                                  .local()
+                                  .format("MMM Do, YYYY h:mm:ss a")}
                               </span>
                             </span>
                           </div>
