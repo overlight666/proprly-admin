81ec9a1f7972ef406d1378be67b2bc4d02b0e516
Herbert Asis <herbert.asis@gcash.com>
2025-02-14 09:44:53 +0800

    remove buttons

--- src/pages/projects/report/reportParent.tsx
+++ src/pages/projects/report/reportParent.tsx
@@ -1,6 +1,4 @@
-import { Accordion, Button } from "flowbite-react";
-import { updateProjectTabMain } from "../../../store/features/appSlice";
-import { useDispatch } from "react-redux";
+import { Accordion } from "flowbite-react";
 import PropertyHeaderReport from "../../../components/propertyHeaderReport";
 import ProjectReportTableNew from "./projectReportTableNew";
 import PropertyReportHeader from "../../../components/propertyReportHeader";
@@ -10,7 +8,6 @@ import CommonAreaHeader from "../../../components/commonAreaHeader";
 import CommonAreaReportTable from "../../../components/commonAreaReportTable";
 
 export default function ConfigureAccordionReports() {
-  const dispatch = useDispatch();
   const [headerValue, setHeaderValue] = useState("all");
   const [headerValue2, setHeaderValue2] = useState("all");
 
@@ -39,25 +36,6 @@ export default function ConfigureAccordionReports() {
           </Accordion.Content>
         </Accordion.Panel>
       </Accordion>
-      <div className="my-5 flex flex-row gap-5">
-        <Button
-          className="w-[200px]"
-          onClick={() => {
-            dispatch(updateProjectTabMain(0));
-          }}
-        >
-          <div className="flex items-center gap-x-2 text-xs">Submit</div>
-        </Button>
-        <Button
-          className="w-[100px]"
-          color="gray"
-          onClick={() => {
-            dispatch(updateProjectTabMain(0));
-          }}
-        >
-          <div className="flex items-center gap-x-2 text-xs">Cancel</div>
-        </Button>
-      </div>
     </div>
   );
 }
