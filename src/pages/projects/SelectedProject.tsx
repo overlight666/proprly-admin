a8deb88420cbb35addb1565c2d08a9be86d0e1bf
Herbert Asis <herbert.asis@gcash.com>
2025-03-03 09:36:49 +0800

    add appointments

--- src/pages/projects/SelectedProject.tsx
+++ src/pages/projects/SelectedProject.tsx
@@ -39,6 +39,7 @@ import DefectResolution from "../DefectResolution/DefectResolution";
 import { Reports } from "../Reports/Reports";
 
 import { Users } from "../users/Users";
+import Appointments from "../Appointments/Appointments";
 
 export default function SelectedProject() {
   const { id, project_id }: any = useParams();
