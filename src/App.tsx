72340a2bd97bf1a011e4059e20d243625b0064db
Herbert Asis <herbert.asis@gcash.com>
2025-03-03 09:30:59 +0800

    fix module

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import Organization from "./pages/Organizations/Organization";
 import AddOrganization from "./pages/Organizations/AddOrganization";
 import { Tooltip } from "react-tooltip";
 import SelectedOrganization from "./pages/Organizations/SelectedOrganization";
-import SelectedProject from "./pages/Projects/SelectedProject";
 import AddProject from "./pages/Projects/AddProject";
 import EditProject from "./pages/Projects/editProject";
 import AddProperty from "./pages/properties/AddProperty";
 import EditProperty from "./pages/properties/EditProperty";
+import SelectedProject from "./pages/Projects/SelectedProject";
 
 export default function App() {
   return (
