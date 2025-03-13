f7aae8aa8aaed5dd5b9033f80d375b7ba7b7af96
Herbert Asis <herbert.asis@gcash.com>
2025-03-13 12:08:43 +0800

    update roles

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -228,8 +228,8 @@ const AppSidebar: React.FC = () => {
       currentRoute === "signup-leads/view" ||
       currentRoute === "/master-configuration" ||
       currentRoute === "/support-tickets" ||
-      currentRoute === "/organization/:id/edit" ||
-      currentRoute === "/organization/:id"
+      currentRoute === "/organization/:id/edit"
+      // currentRoute === "/organization/:id"
     ) {
       setShowProjects(false);
       const navItemsHolder: NavItem[] = [
