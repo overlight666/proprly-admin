730da941a0ad96d2aea8a29a94b2b9ec9241c7d1
Herbert Asis <herbert.asis@gcash.com>
2025-03-30 15:48:50 +0800

    hide sc

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -595,7 +595,7 @@ const AppSidebar: React.FC = () => {
           )}
         </Link>
       </div>
-      <div className="flex flex-col overflow-y-auto duration-300 ease-linear">
+      <div className="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
         <nav className="mb-6">
           <div className="flex flex-col gap-4">
             <div>
