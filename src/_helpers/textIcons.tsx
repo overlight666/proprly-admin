05363a4745cfa3b19829791744b9914eb5a58add
Herbert Asis <herbert.asis@gcash.com>
2025-04-02 12:28:00 +0800

    accepted

--- src/_helpers/textIcons.tsx
+++ src/_helpers/textIcons.tsx
@@ -13,6 +13,7 @@ export const ticketColoring = (defectSubStatus: any, hasBg: any) => {
     return hasBg ? "text-red-800 bg-red-100" : "text-red-400";
   } else if (
     defectSubStatus?.toLowerCase() == "resolved" ||
+    defectSubStatus?.toLowerCase() == "accepted" ||
     defectSubStatus == "Defect Resolved" ||
     defectSubStatus == "Resolution Accepted by Auditor" ||
     defectSubStatus == "Resolution Accepted By Owner" ||
@@ -59,6 +60,7 @@ export const textColoring = (defectSubStatus: any, hasBg: any) => {
     return hasBg ? "text-red-800 bg-red-100" : "text-red-400";
   } else if (
     defectSubStatus?.toLowerCase() == "resolved" ||
+    defectSubStatus?.toLowerCase() == "accepted" ||
     defectSubStatus == "Defect Resolved" ||
     defectSubStatus == "Resolution Accepted by Auditor" ||
     defectSubStatus == "Resolution Accepted By Owner" ||
@@ -149,6 +151,7 @@ export const getIcons = (defectSubStatus) => {
     );
   } else if (
     defectSubStatus?.toLowerCase() == "resolved" ||
+    defectSubStatus?.toLowerCase() == "accepted" ||
     defectSubStatus == "Defect Resolved" ||
     defectSubStatus == "Resolution Accepted by Auditor" ||
     defectSubStatus == "Resolution Accepted By Owner" ||
