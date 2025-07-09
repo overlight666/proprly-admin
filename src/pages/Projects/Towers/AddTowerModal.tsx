d231a332d85514e66db00aa7915cb848bf46011e
Herbert Asis <herbert.asis@gcash.com>
2025-07-09 14:49:17 +0800

    update floors

--- src/pages/Projects/Towers/AddTowerModal.tsx
+++ src/pages/Projects/Towers/AddTowerModal.tsx
@@ -13,48 +13,11 @@ export default function AddTowerModal({ isOpen, closeModal, addTower }: any) {
 
   const [nameError, setNameError] = useState<any>("");
 
-  const towerOptions: any = [
-    {
-      label: 1,
-      value: 1,
-    },
-    {
-      label: 2,
-      value: 2,
-    },
-    {
-      label: 3,
-      value: 3,
-    },
-    {
-      label: 4,
-      value: 4,
-    },
-    {
-      label: 5,
-      value: 5,
-    },
-    {
-      label: 6,
-      value: 6,
-    },
-    {
-      label: 7,
-      value: 7,
-    },
-    {
-      label: 8,
-      value: 8,
-    },
-    {
-      label: 9,
-      value: 9,
-    },
-    {
-      label: 10,
-      value: 10,
-    },
-  ];
+  const towerOptions: any = [...Array(50)].map((i, index) => ({
+    label: index + 1,
+    value: index + 1,
+  }));
+
 
   function onSubmit() {
     setNameError("");
